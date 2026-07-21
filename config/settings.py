ae9bf78cda55b1ef98891c4a9f1570cd6e7ffb7e
bhishmraj010 <yogansh37@gmail.com>
2026-07-21 17:27:49 +0530

    Configure WhiteNoise for static files in production

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,7 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -176,6 +177,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CHANNEL_LAYERS = {
     "default": {
